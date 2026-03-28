1f21019433baae086bdd713580cf63db142e73dd
Niam3231 <niam3231@hotmail.com>
2026-03-28 22:54:23 +0100

    Create startx-installer.sh

---
+++ startx-installer.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+echo "[*] Installing minimal X server..."
+apt update
+apt install -y xorg x11-xserver-utils xinit xterm
+echo "[*] Configuring ~/.xinitrc..."
+cat > ~/.xinitrc <<'EOF'
+#!/bin/sh
+xsetroot -solid black
+xsetroot -cursor_name cross
+exec xterm
+EOF
+chmod +x ~/.xinitrc
+echo "[*] Setup complete. Just run 'startx' to launch minimal X."
+echo "You will see a black screen with crosshair and xterm terminal. Launch graphical apps from xterm or another TTY with: DISPLAY=:0 appname &"
